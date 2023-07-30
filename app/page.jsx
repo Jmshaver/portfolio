88df3ccf308260992da7422864d83b03b0086f87
jmshaver <jmshaver@umich.edu>
2023-07-30 15:48:51 -0500

    basic

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,32 @@
-import Hero from "@/components/hero"
-
+import Hero from '@/components/hero';
+import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <Hero>
-
-      </Hero>
+      <Hero />
+      <section className="container mx-auto px-2">
+        <div class="mockup-window bg-base-300">
+          <Image
+            className=""
+            width={1920}
+            height={1080}
+            src="/claricity.consulting.png"
+            alt="Me, Jay Shaver"
+          />
+        </div>
+        <div class="mockup-phone">
+          <div class="camera"></div>
+          <div class="display">
+            <Image
+              className="artboard artboard-demo phone-4"
+              width={390}
+              height={844}
+              src="/claricity.consulting_(iPhone 12 Pro).png"
+              alt="Me, Jay Shaver"
+            />
+          </div>
+        </div>
+      </section>
     </>
-
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-radial':
+          'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        'primary': '',
-        'secondary': ''
-      }
+        primary: '',
+        secondary: '',
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require('daisyui')],
+};
